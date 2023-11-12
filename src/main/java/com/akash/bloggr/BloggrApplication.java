ed3c33a22de8918eae9a30263f17c5ed84dfb541
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-11-12 11:54:23 +0530

    [init] initialized the application

---
+++ src/main/java/com/akash/bloggr/BloggrApplication.java
@@ -0,0 +1,13 @@
+package com.akash.bloggr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/bloggr/BloggrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.bloggr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
