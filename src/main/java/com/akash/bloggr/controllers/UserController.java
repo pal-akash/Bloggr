41d277a9fa87b5cc2522d53be8995d2ad9d85dc1
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-12-08 16:17:35 +0530

    create api for CRUD ops

--- src/main/java/com/akash/bloggr/controllers/UserController.java
+++ src/main/java/com/akash/bloggr/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.akash.bloggr.controllers;
 
+import com.akash.bloggr.payloads.ApiResponse;
 import com.akash.bloggr.payloads.UserDto;
 import com.akash.bloggr.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,4 +23,25 @@ public class UserController {
         return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
     }
 
+    @PutMapping("/userId")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Long userId){
+        UserDto updatedUser = userService.updateUser(userDto, userId);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/userId")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId){
+        userService.deleteUser(userId);
+        return new ResponseEntity<>(new ApiResponse("User deleted successfully", true), HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/userId")
+    public ResponseEntity<UserDto> getSingleUser(@PathVariable Long userId){
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
 }
